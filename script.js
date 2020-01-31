729357ca2bf562cf868d356da74763405a9654d5
Ollones741 <31546649+Ollones741@users.noreply.github.com>
2020-01-31 19:02:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,41 +7,84 @@ const renderQuestions = object => `
             <a href="#" class="card-link">${object.author}</a>
          </div>
       </div>`;
+      
+const renderMenu = param => `
+      <button type="button" class="btn btn-secondary btn-block" data-subject='${param.subject}'>${param.subject}</button>
+      `;
 
-const menuItems = document.querySelectorAll('#menu button');
+const renderForm = value => `
+      <option value="${object.subject}">${object.subject}</option>
+      `;
 
-const loadQuestions = (event) => {
-   console.log(event.target.dataset.course)
-   const course = event.target.dataset.course;
+const loadMenu = (event) => {
    createRequest({ path: `/api/v001/questions`, method: "GET"})
     .then(response => {
-       document.querySelector('#body').innerHTML = response
-       .map(renderQuestions)
+       document.querySelector('#menu').innerHTML = response.questions
+       .map(renderMenu)
        .join("");
+       const menuItems = document.querySelectorAll('#menu button');
+       menuItems.forEach(item => {
+         item.addEventListener('click', loadQuestions)
+       })
        console.log("Результат вопросов", response);
     });
 };
 
-menuItems.forEach(item => {
-   item.addEventListener('click', loadQuestions)
-})
+const loadQuestions = (event) => {
+   console.log(event.target.dataset.subject)
+   createRequest({ path: `/api/v001/questions?subject=${event.target.dataset.subject}`, method: "GET"})
+    .then(response => {
+         document.querySelector('#body').innerHTML = response.questions
+         .map(renderQuestions)
+         .join("");
+    });
+};
+
+const loadAllQuestions = (event) => {
+   console.log(event.target.dataset.subject)
+   createRequest({ path: `/api/v001/questions`, method: "GET"})
+    .then(response => {
+         document.querySelector('#allsub').innerHTML = response.questions
+         .map(renderQuestions)
+         .join("");
+         document.querySelector('#allsub').addEventListener('click', loadAllQuestions);
+    });
+};
+
+const loadForm = (event) => {
+   console.log(event.target.dataset.subject)
+   createRequest({ path: `/api/v001/questions?subject=${event.target.dataset.subject}`, method: "GET"})
+    .then(response => {
+         document.querySelector('#form').innerHTML = response.questions
+         .map(renderForm)
+         .join("");
+    });
+};
+
+document.addEventListener('DOMContentLoaded', loadMenu);
 
 const addQuestionForm = document.querySelector('#addQuestion');
 addQuestionForm.addEventListener("submit", event => {
    event.preventDefault();
    
-   const data = getFieldData(event.Target);
+   const data = getFieldData(event.target);
    console.log("main", "data", data);
    
    toggleClass(".addQuestion", "loading")
    
-   createRequest({ path: `/api/v001/books`, method: "POST" }, {}, data)
+   createRequest({ path: `/api/v001/questions?subject=${event.target.dataset.subject}`, method: "POST" }, {}, data)
     .then(response => {
       toggleClass(".addQuestion", "loading");
       console.log("Вопрос добавлен", response);
+      loadQuestions;
     })
     .catch(() => {
       toggleClass(".addQuestion", "loading");
       console.log("Не удалось добавить вопрос");
     });
+});
+
+const userSelector = document.querySelector('.select_control-user');
+userSelector.addEventListener('change', event => {
+  userId = event.target.value;
 });
